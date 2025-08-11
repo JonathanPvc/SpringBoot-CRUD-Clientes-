a403a1c7ec701f35169e1148846fce38281463d3
Jonathan Uribe <jonathan13pvc@gmail.com>
2025-08-11 15:26:11 -0500

    URI a mis respuestas
    load

--- src/main/java/com/Curso10Springboot/Curso/Controllers/CustomerRestController.java
+++ src/main/java/com/Curso10Springboot/Curso/Controllers/CustomerRestController.java
@@ -4,7 +4,9 @@ import com.Curso10Springboot.domain.Customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.rmi.server.RemoteRef;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -62,8 +64,17 @@ public class CustomerRestController {
     //@RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<?> postCliente( @RequestBody  Customer customer) {
         customers.add(customer);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Customer created successfully: " + customer.getName());
+        //apliquemos buenas practicas
+       // return ResponseEntity.status(HttpStatus.CREATED).body("Customer created successfully: " + customer.getName());
 
+         URI location = ServletUriComponentsBuilder
+                 .fromCurrentRequest()
+                 .path("/{username}")
+                 .buildAndExpand(customer.getUsername())
+                 .toUri();
+
+         //return ResponseEntity.created(location).build();
+         return ResponseEntity.created(location).body(customer);
     }
 
 
