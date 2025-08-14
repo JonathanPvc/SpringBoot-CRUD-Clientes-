846b7fb1fea88721f30f4c796177a255aa8dd336
Jonathan Uribe <jonathan13pvc@gmail.com>
2025-08-14 12:21:28 -0500

    Video 30  final de curso
    prácticamente variables de entorno

--- src/main/java/com/Curso10Springboot/Curso/Controllers/ProductController.java
+++ src/main/java/com/Curso10Springboot/Curso/Controllers/ProductController.java
@@ -1,4 +1,5 @@
 package com.Curso10Springboot.Curso.Controllers;
+import com.Curso10Springboot.Curso.configurations.ExternalizedConfigurations;
 import com.Curso10Springboot.Curso.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Curso10Springboot.Curso.services.ProductService;
 
 import java.util.List;
-@Primary
+//@Primary
 @RestController
 @RequestMapping("/products")
 public class ProductController {
@@ -21,8 +22,16 @@ public class ProductController {
     //@Qualifier("jsonResourceService")
     private ProductService productService;
 
+
+
+    @Autowired
+    private ExternalizedConfigurations ExternalizedConfigurations;
+
     @GetMapping
     public ResponseEntity <?> getProducts() {
+
+        System.out.println(ExternalizedConfigurations.toString());
+
         List<Product> products = productService.getProducts();
 
         return ResponseEntity.ok(products);
---
+++ src/main/java/com/Curso10Springboot/Curso/configurations/ExternalizedConfigurations.java
@@ -0,0 +1,73 @@
+package com.Curso10Springboot.Curso.configurations;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+//@ConditionalOnProperty(prefix = "app", name = "enabled", havingValue = "true")
+public class ExternalizedConfigurations {
+
+    private  String name;
+    private  String version;
+    private  String author;
+    private  String language;
+    private  String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "ExternalizedConfigurations{" +
+                "name='" + name + '\n' +
+                ", version='" + version + '\n' +
+                ", author='" + author + '\n' +
+                ", language='" + language + '\n' +
+                ", country='" +  country    + '\n' +
+                '}';
+    }
+
+}
